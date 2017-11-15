1ddcf3ed52dfe1126e9472021b7f2a5405d50bf1
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-15 18:16:46 -0500

    favourites WORK! :D

--- server.rb
+++ server.rb
@@ -12,6 +12,10 @@ end
 get '/about_me' do
   @skills = ['BEING AWESOME', 'baking', 'ruby', 'git', 'html']
   @interests = ['comic book movies', 'detective fiction', 'music', 'tea']
-
   erb :details
+end
+
+get '/favourites' do
+  @favourites = {"Twitter" => "https://twitter.com/", "BBC News" => "http://www.bbc.com/news", "Toronto Star" => "https://www.thestar.com/", "Vulture" => "http://www.vulture.com/", "AV Club" =>"https://www.avclub.com/"}
+  erb :favourites
 end
